8ecf3d57c1a74e7d2490d16fa54b6593411de0d0
ParthAggarwal1404 <141123480+ParthAggarwal1404@users.noreply.github.com>
2023-08-13 23:53:44 +0530

    Add files via upload

---
+++ Comparing_two_number_using_array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int arr[2],larg1,larg2,temp,i;
+    for(i=0;i<2;i++)
+    scanf("%d",&arr[i]);
+    larg1= arr[0];
+
+    larg2 = arr[1];
+    if(larg2>larg1) {
+        
+    }
+}
